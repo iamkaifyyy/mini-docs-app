a21c8b1d94145fbc6e569a43d9b567b5baa95a95
kaifyyy <mkaifm728@gmail.com>
2025-10-03 03:56:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-800'>
-      <h1>Docs</h1>
+    <div className='relative w-full h-screen bg-zinc-800'>
+
+      <nav>
+        
+      </nav>
+
+      <h1 className='text-[13vw] leading-none top-1/2 left-1/2 tracking-tighter font-semibold absolute -translate-x-[50%] -translate-y-[50%] text-zinc-900'>Docs.</h1>
       
     </div>
   )
