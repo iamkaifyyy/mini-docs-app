89339ae74d667aec17f74298b20644efe1d80fe4
kaifyyy <mkaifm728@gmail.com>
2025-10-03 04:06:12 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Background from  './components/Background';
 
 const App = () => {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-
-      <nav>
-        
-      </nav>
-
-      <h1 className='text-[13vw] leading-none top-1/2 left-1/2 tracking-tighter font-semibold absolute -translate-x-[50%] -translate-y-[50%] text-zinc-900'>Docs.</h1>
+     
       
+
+
+
+
+      <Background />
     </div>
   )
 }
