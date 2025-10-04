557e0c2e506c2b3ed260d46d4b37f187c5103867
kaifyyy <mkaifm728@gmail.com>
2025-10-04 13:25:07 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,11 @@
-<div className='w-60 h-72 bg-zinc-900/90 rounded-[20px]'></div>
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-60 h-72 bg-zinc-900/90 rounded-[20px]'>
+        
+    </div>
+  )
+}
+
+export default Card
