68289733bc395219b6089ce93da09d2320592f45
kaifyyy <mkaifm728@gmail.com>
2025-10-04 13:34:10 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
 
-const Card = () => {
+export const Card = () => {
   return (
-    <div className='w-60 h-72 bg-zinc-900/90 rounded-[20px]'>
-        
+    <div className='relative w-60 h-72 rounded-[20px] bg-zinc-900/90 text-white p-5'>
+        <FaRegFileAlt />
+        <p className='text-sm mt-5 font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    
     </div>
   )
 }
-
-export default Card
