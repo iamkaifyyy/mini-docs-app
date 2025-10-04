75cf2d1fe4966c25b34553fc1789207adb3b8df9
kaifyyy <mkaifm728@gmail.com>
2025-10-04 14:46:45 +0530

    commit

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
-import Card from './Card'
+import React, { useState } from "react";
+import Card from "./Card";
 
 const Foreground = () => {
-  
+  const data = [
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColoe: "green" },
+    },
+  ];
   return (
-    <div className='w-full h-full fixed  z-[3] top-0 left-0 '>
+    <div className="w-full h-full fixed  z-[3] top-0 left-0 ">
+      {data.map((item, index)=>(
+        <Card data={item}/>
+      )) }
 
-
-
-      <Card />   
+      
     </div>
-  )
-}
+  );
+};
 
-export default Foreground
+export default Foreground;
